7edf2dbebe5a9ef68cc5ee19a0941e10fa182502
yagerRVP <j69roboman@gmail.com>
2025-05-19 17:00:29 -0500

    Create promotionRules.js

---
+++ src/logic/promotionRules.js
@@ -0,0 +1,20 @@
+const promotionRules = {
+  seniorRep: {
+    recruits: 3,
+    production: 3000,
+  },
+  district: {
+    recruits: 5,
+    licenses: 5,
+    teamVolume: 10000,
+  },
+  division: {
+    licensedLeaders: 3,
+    teamVolume: 15000,
+  },
+  rvp: {
+    manualApproval: true, // Only James Yager can promote
+  },
+};
+
+module.exports = promotionRules;
