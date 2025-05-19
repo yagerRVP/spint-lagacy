3fea62593ae51a444887a9d8f1b48507b1df8068
yagerRVP <j69roboman@gmail.com>
2025-05-19 17:19:47 -0500

    Create Dashboard.js

---
+++ src/logic/src/logic/src/logic/src/logic/src/logic/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Your Leadership Dashboard</h1>
+      <p>Select a rep or view progress to get started.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
